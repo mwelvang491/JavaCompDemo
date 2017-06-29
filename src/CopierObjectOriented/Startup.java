bf1c5494d1226070a44fd0cd6ae3e4158b28a191
Mitch W <Mitch W@192.168.1.64>
2017-06-29 18:05:18 -0500

    Added Comments.

--- src/CopierObjectOriented/Startup.java
+++ src/CopierObjectOriented/Startup.java
@@ -13,10 +13,12 @@ public class Startup {
    // Input From Input Class.
    // Use Copier Class to Pass Input To Output Class.
    // Output with Output Class.
+   
    // InputStrategy input = new InputConsole("Input: ");
       InputStrategy input = new InputGui("Input: ");
       
-      OutputGui output = new OutputGui("Output: ");
+  //OutputGui output = new OutputGui("Output: ");
+      OutputStrategy output = new OutputConsole("Output: ");
       Copier copier = new Copier();
       
       copier.copy(input, output);
